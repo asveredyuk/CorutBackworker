72eeadcf1c22210c4c0381b8b047d0af41f88138
asveredyuk <alexsv1802@gmail.com>
2016-03-11 00:13:40 +0300

    Form improved
    Added some features to form, like bind roles

--- project/CorutinesWorker/SimpleProgressForm.cs
+++ project/CorutinesWorker/SimpleProgressForm.cs
@@ -13,13 +13,19 @@ namespace CorutinesWorker
 {
     public partial class SimpleProgressForm : Form
     {
+        /// <summary>
+        /// Bind role - none (ex. hides label), show progress or show text messages
+        /// </summary>
+        public enum TextBindRole {None, Progress, Text}
+
         public bool askWhenClosed = true;
         public bool closeAfterFinished = true;
         public bool showCompletedDialog = true;
         public bool showCancelledDialog = true;
+        public TextBindRole titleBindRole = TextBindRole.None;
+        public TextBindRole labelBindRole = TextBindRole.Progress;
         public string title = "Progress";
-        //string title = "Progress",bool askWhenClosed = true, bool closeAfterFinished = true, bool showCompletedDialog = true, bool showCancelledDialog = true
-        Corutine corutine;
+        private Corutine corutine;
         public SimpleProgressForm(Corutine corut)
         {
             InitializeComponent();
@@ -29,10 +35,23 @@ namespace CorutinesWorker
         private void SimpleProgressForm_Load(object sender, EventArgs e)
         {
             this.Text = title;
+            //check if we do not need label
+            if (labelBindRole == TextBindRole.None)
+                label.Visible = false;
+            //bind progress bar to corutine
             corutine.BindProgressBar(progressBar);
-            corutine.BindText(label);
-            corutine.BindProgressText(this);
-            
+            //bind label to corutine, if it is enabled
+            if(labelBindRole == TextBindRole.Text)
+                corutine.BindText(label);
+            if(labelBindRole == TextBindRole.Progress)
+                corutine.BindProgressText(label);
+
+            //bind this form to corutine, if enabled
+            if(titleBindRole == TextBindRole.Progress)
+                corutine.BindProgressText(this);
+            if(titleBindRole == TextBindRole.Text)
+                corutine.BindText(this);
+            //on completed event
             corutine.OnCompleted(delegate(object res)
             {
                 if (showCompletedDialog)
@@ -40,6 +59,7 @@ namespace CorutinesWorker
                 if (closeAfterFinished)
                     this.ForceClose();
             });
+            //on cancelled event
             corutine.OnCancelled(delegate()
             {
                 if (showCancelledDialog)
@@ -47,32 +67,37 @@ namespace CorutinesWorker
                 if (closeAfterFinished)
                     this.ForceClose();
             });
-
-
             corutine.Start();
         }
-
         private void buttonCancel_Click(object sender, EventArgs e)
         {
             corutine.Cancel();
         }
+        /// <summary>
+        /// Start corutine
+        /// </summary>
         public void Start()
         {
             this.ShowDialog();
         }
+        /// <summary>
+        /// Used for closing form without any messagebox
+        /// </summary>
         bool forceClose = false;
-
+        /// <summary>
+        /// Close form without any notifications
+        /// </summary>
         private void ForceClose()
         {
             forceClose = true;
             this.Close();
         }
+
         private void SimpleProgressForm_FormClosing(object sender, FormClosingEventArgs e)
         {
 
             if (forceClose)
                 return;
-            
             if(corutine.IsWorking)
             {
                 if (askWhenClosed)
